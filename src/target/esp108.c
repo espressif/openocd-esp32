4a4ac2273c742411765865d626f5ff2fa8cf872e
Jeroen Domburg <git@j0h.nl>
2016-04-06 15:03:17 +0800

    Add trace register defines

--- src/target/esp108.c
+++ src/target/esp108.c
@@ -120,7 +120,6 @@ it does not affect OCD in any way.
 #define TAPINS_IDCODE_LEN		32
 #define TAPINS_BYPASS_LEN		1
 
-
 /* 
  From the manual:
  To properly use Debug registers through JTAG, software must ensure that:
@@ -238,13 +237,59 @@ it does not affect OCD in any way.
 #define OCDDSR_BREACKOUTACKITI	(1<<25)
 #define OCDDSR_BREAKINITI		(1<<26)
 
+#define DEBUGCAUSE_IC			(1<<0)	//ICOUNT exception
+#define DEBUGCAUSE_IB			(1<<1)	//IBREAK exception
+#define DEBUGCAUSE_DB			(1<<2)	//DBREAK exception
+#define DEBUGCAUSE_BI			(1<<3)	//BREAK instruction encountered
+#define DEBUGCAUSE_BN			(1<<4)	//BREAK.N instruction encountered
+#define DEBUGCAUSE_DI			(1<<5)	//Debug Interrupt
 
-#define DEBUGCAUSE_IC			(1<<0)
-#define DEBUGCAUSE_IB			(1<<1)
-#define DEBUGCAUSE_DB			(1<<2)
-#define DEBUGCAUSE_BI			(1<<3)
-#define DEBUGCAUSE_BN			(1<<4)
-#define DEBUGCAUSE_DI			(1<<5)
+#define TRAXCTRL_TREN			(1<<0)	//Trace enable. Tracing starts on 0->1
+#define TRAXCTRL_TRSTP			(1<<1)	//Trace Stop. Make 1 to stop trace.
+#define TRAXCTRL_PCMEN			(1<<2)	//PC match enable
+#define TRAXCTRL_PTIEN			(1<<4)	//Processor-trigger enable
+#define TRAXCTRL_CTIEN			(1<<5)	//Cross-trigger enable
+#define TRAXCTRL_TMEN			(1<<7)	//Tracemem Enable. Always set.
+#define TRAXCTRL_CNTU			(1<<9)	//Post-stop-trigger countdown units; selects when DelayCount-- happens.
+										//0 - every 32-bit word written to tracemem, 1 - every cpu instruction
+#define TRAXCTRL_TSEN			(1<<11)	//Undocumented/deprecated?
+#define TRAXCTRL_SMPER_SHIFT	12		//Send sync every 2^(9-smper) messages. 7=reserved, 0=no sync msg
+#define TRAXCTRL_SMPER_MASK		0x7		//Synchronization message period
+#define TRAXCTRL_PTOWT			(1<<16) //Processor Trigger Out enabled when stop triggered
+#define TRAXCTRL_PTOWS			(1<<17) //Processor Trigger Out enabled when trace stop completes
+#define TRAXCTRL_CTOWT			(1<<20) //Cross-trigger Out enabled when stop triggered
+#define TRAXCTRL_CTOWS			(1<<21) //Cross-trigger Out enabled when trace stop completes
+#define TRAXCTRL_ITCTO			(1<<22) //Integration mode: cross-trigger output
+#define TRAXCTRL_ITCTIA			(1<<23) //Integration mode: cross-trigger ack
+#define TRAXCTRL_ITATV			(1<<24) //replaces ATID when in integration mode: ATVALID output
+#define TRAXCTRL_ATID_MASK		0x7F	//ARB source ID
+#define TRAXCTRL_ATID_SHIFT		24
+#define TRAXCTRL_ATEN			(1<<31) //ATB interface enable
+
+#define TRAXSTAT_TRACT			(1<<0)	//Trace active flag.
+#define TRAXSTAT_TRIG			(1<<1)	//Trace stop trigger. Clears on TREN 1->0
+#define TRAXSTAT_PCMTG			(1<<2)	//Stop trigger caused by PC match. Clears on TREN 1->0
+#define TRAXSTAT_PJTR			(1<<3)	//JTAG transaction result. 1=err in preceding jtag transaction.
+#define TRAXSTAT_PTITG			(1<<4)	//Stop trigger caused by Processor Trigger Input. Clears on TREN 1->0
+#define TRAXSTAT_CTITG			(1<<5)	//Stop trigger caused by Cross-Trigger Input. Clears on TREN 1->0
+#define TRAXSTAT_MEMSZ_SHIFT	8		//Traceram size inducator. Usable trace ram is 2^MEMSZ bytes.
+#define TRAXSTAT_MEMSZ_MASK		0x1F
+#define TRAXSTAT_PTO			(1<<16) //Processor Trigger Output: current value
+#define TRAXSTAT_CTO			(1<<17) //Cross-Trigger Output: current value
+#define TRAXSTAT_ITCTOA			(1<<22) //Cross-Trigger Out Ack: current value
+#define TRAXSTAT_ITCTI			(1<<23) //Cross-Trigger Input: current value
+#define TRAXSTAT_ITATR			(1<<24) //ATREADY Input: current value
+
+#define TRAXADDR_TADDR_SHIFT	0		//Trax memory address, in 32-bit words.
+#define TRAXADDR_TADDR_MASK		0x1FFFFF //Actually is only as big as the trace buffer size max addr.
+#define TRAXADDR_TWRAP_SHIFT	21		//Amount of times TADDR has overflown
+#define TRAXADDR_TWRAP_MASK		0x3FF
+#define TRAXADDR_TWSAT			(1<<31) //1 if TWRAP has overflown, clear by disabling tren.
+
+#define PCMATCHCTRL_PCML_SHIFT	0		//Amount of lower bits to ignore in pc trigger register
+#define PCMATCHCTRL_PCML_MASK	0x1F 
+#define PCMATCHCTRL_PCMS		(1<<31) //PC Match Sense, 0 - match when procs PC is in-range, 1 - match when
+										//out-of-range
 
 #define XT_INS_NUM_BITS 24
 #define XT_DEBUGLEVEL    6 /* XCHAL_DEBUGLEVEL in xtensa-config.h */
@@ -1742,6 +1787,7 @@ static const struct command_registration esp108_any_command_handlers[] = {
 		.help = "Set the way the CPU chains OCD breaks",
 		.usage = "[none|breakinout|runstall] | [BreakIn] [BreakOut] [RunStallIn] [DebugModeOut]",
 	},
+
 	COMMAND_REGISTRATION_DONE
 };
 
