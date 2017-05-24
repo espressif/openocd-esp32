675970f44f5868b732471e2c772edcfc0f87914e
Ivan Grokhotkov <ivan@espressif.com>
2017-05-24 12:30:27 +0800

    esp32, esp108: change thread_name offset to 56

--- src/rtos/FreeRTOS.c
+++ src/rtos/FreeRTOS.c
@@ -111,26 +111,26 @@ static const struct FreeRTOS_params FreeRTOS_params_list[] = {
 	8,						/* list_elem_next_offset; */
 	12,						/* list_elem_content_offset */
 	0,						/* thread_stack_offset; */
-	60,						/* thread_name_offset; */
+	56,						/* thread_name_offset; */
 	NULL,					/* stacking_info */
 	&rtos_standard_Cortex_M4F_stacking,
 	&rtos_standard_Cortex_M4F_FPU_stacking,
 	rtos_freertos_esp108_pick_stacking_info, /* fn to pick stacking_info */
 	},
 	{
-		"esp32",				/* target_name */
-		4,						/* thread_count_width; */
-		4,						/* pointer_width; */
-		16,						/* list_next_offset; */
-		20,						/* list_width; */
-		8,						/* list_elem_next_offset; */
-		12,						/* list_elem_content_offset */
-		0,						/* thread_stack_offset; */
-		60,						/* thread_name_offset; */
-		NULL,					/* stacking_info */
-		&rtos_standard_Cortex_M4F_stacking,
-		&rtos_standard_Cortex_M4F_FPU_stacking,
-		rtos_freertos_esp108_pick_stacking_info, /* fn to pick stacking_info */
+	"esp32",				/* target_name */
+	4,						/* thread_count_width; */
+	4,						/* pointer_width; */
+	16,						/* list_next_offset; */
+	20,						/* list_width; */
+	8,						/* list_elem_next_offset; */
+	12,						/* list_elem_content_offset */
+	0,						/* thread_stack_offset; */
+	56,						/* thread_name_offset; */
+	NULL,					/* stacking_info */
+	&rtos_standard_Cortex_M4F_stacking,
+	&rtos_standard_Cortex_M4F_FPU_stacking,
+	rtos_freertos_esp108_pick_stacking_info, /* fn to pick stacking_info */
 	},
 
 };
