866cf69c5bd1d30fc5a5df86dfb89e05a8d0d944
Marc Schink <openocd-dev@marcschink.de>
2017-10-29 11:39:51 +0100

    rtos: Use 'bool' as return type for detect_rtos()
    Change-Id: I91ad0431d44ed94f48d20c4690f8642d66f52a9b
    Signed-off-by: Marc Schink <openocd-dev@marcschink.de>
    Reviewed-on: http://openocd.zylin.com/4274
    Tested-by: jenkins
    Reviewed-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>

--- src/rtos/FreeRTOS.c
+++ src/rtos/FreeRTOS.c
@@ -142,7 +142,7 @@ static const struct FreeRTOS_params FreeRTOS_params_list[] = {
 
 #define FREERTOS_NUM_PARAMS ((int)(sizeof(FreeRTOS_params_list)/sizeof(struct FreeRTOS_params)))
 
-static int FreeRTOS_detect_rtos(struct target *target);
+static bool FreeRTOS_detect_rtos(struct target *target);
 static int FreeRTOS_create(struct target *target);
 static int FreeRTOS_update_threads(struct rtos *rtos);
 static int FreeRTOS_get_thread_reg_list(struct rtos *rtos, int64_t thread_id, char **hex_reg_list);
@@ -683,7 +683,6 @@ static int FreeRTOS_get_thread_ascii_info(struct rtos *rtos, threadid_t thread_i
 
 #endif
 
-
 static int FreeRTOS_post_reset_cleanup(struct target *target)
 {
 	LOG_DEBUG("FreeRTOS_post_reset_cleanup");
@@ -724,14 +723,14 @@ static int FreeRTOS_clean(struct target *target)
 	return ERROR_OK;
 }
 
-static int FreeRTOS_detect_rtos(struct target *target)
+static bool FreeRTOS_detect_rtos(struct target *target)
 {
 	if ((target->rtos->symbols != NULL) &&
 			(target->rtos->symbols[FreeRTOS_VAL_pxReadyTasksLists].address != 0)) {
 		/* looks like FreeRTOS */
-		return 1;
+		return true;
 	}
-	return 0;
+	return false;
 }
 
 static int FreeRTOS_create(struct target *target)
