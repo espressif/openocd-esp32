2659e4e4b24283bab9e67da6b9a0f11c05e5f468
Marek Vasut <marek.vasut@gmail.com>
2019-04-13 22:44:45 +0200

    flash/nor: Add Renesas RPC HF driver
    Add driver for the RPC block in HF mode on Renesas R-Car Gen3 SoCs.
    This driver allows operating the on-SIP HF memory.
    
    Note that HF is CFI compliant flash, but it is not memory mapped,
    hence the need to replace all the memory accessors and read/write
    functions. The write function is entirely replaced to increase
    performance and is Spansion/AMD specific, since there is no known
    SIP with other HF from another vendor.
    
    Add the following two lines to board TCL file to bind the driver on
    R-Car Gen3 SoC using HyperFlash:
    
      set _FLASHNAME $_CHIPNAME.flash
      flash bank $_FLASHNAME rpchf 0x08000000 0x4000000 2 2 $_CHIPNAME.a57.0
    
    Change-Id: Ie18729d017eeb46e1363333ffe002d010dfc5ead
    Signed-off-by: Marek Vasut <marek.vasut@gmail.com>
    Reviewed-on: http://openocd.zylin.com/5149
    Tested-by: jenkins
    Reviewed-by: Oleksij Rempel <linux@rempel-privat.de>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -70,6 +70,7 @@ extern const struct flash_driver psoc5lp_flash;
 extern const struct flash_driver psoc5lp_eeprom_flash;
 extern const struct flash_driver psoc5lp_nvl_flash;
 extern const struct flash_driver psoc6_flash;
+extern const struct flash_driver renesas_rpchf_flash;
 extern const struct flash_driver sh_qspi_flash;
 extern const struct flash_driver sim3x_flash;
 extern const struct flash_driver stellaris_flash;
@@ -145,6 +146,7 @@ static const struct flash_driver * const flash_drivers[] = {
 	&psoc5lp_eeprom_flash,
 	&psoc5lp_nvl_flash,
 	&psoc6_flash,
+	&renesas_rpchf_flash,
 	&sh_qspi_flash,
 	&sim3x_flash,
 	&stellaris_flash,
