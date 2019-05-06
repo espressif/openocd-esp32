8b8b7281df17c9b9771511c62e222d1e62e0739a
Antonio Borneo <borneo.antonio@gmail.com>
2019-05-06 12:31:57 +0200

    coding style: prototype of functions with no parameters
    Functions that have no parameters should use "void" as parameter
    in the function declaration.
    
    Issue identified and fixed by script checkpatch from Linux kernel
    v5.1 using the command
    
    	find src/ -type f -exec ./tools/scripts/checkpatch.pl \
    	-q --types FUNCTION_WITHOUT_ARGS --fix-inplace -f {} \;
    
    Change-Id: If104ac75b44e939ec86155ff7b5720f2e33c6b39
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: http://openocd.zylin.com/5621
    Reviewed-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>
    Tested-by: jenkins

--- src/jtag/core.c
+++ src/jtag/core.c
@@ -1871,7 +1871,7 @@ void jtag_set_verify(bool enable)
 	jtag_verify = enable;
 }
 
-bool jtag_will_verify()
+bool jtag_will_verify(void)
 {
 	return jtag_verify;
 }
@@ -1881,7 +1881,7 @@ void jtag_set_verify_capture_ir(bool enable)
 	jtag_verify_capture_ir = enable;
 }
 
-bool jtag_will_verify_capture_ir()
+bool jtag_will_verify_capture_ir(void)
 {
 	return jtag_verify_capture_ir;
 }
