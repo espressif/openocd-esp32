78e2eb311dac929961bec64ee73ebcf246a2104c
Alexey Gerenkov <alexey@espressif.com>
2019-11-05 20:10:54 +0300

    testing/esp32: Adds test for flash cache handling

--- testing/esp/test_flasher.py
+++ testing/esp/test_flasher.py
@@ -43,6 +43,23 @@ class FlasherTestsImpl:
         self.gdb.monitor_run('flash read_bank 0 %s 0x%x %d' % (dbg.fixup_path(fname2), ESP32_APP_FLASH_OFF + ESP32_APP_FLASH_SZ, size*1024), tmo=120)
         self.assertTrue(filecmp.cmp(fname1, fname2))
 
+    def test_cache_handling(self):
+        """
+            This test checks that flasher does not corrupts cache config registers when setting breakpoints.
+            The idea is the following: GDB postpones writing breakpoints to resume.
+            So cache config registers should have the same values after resuming with set flash breakpoints.
+            It is checked by the test code on target. This test method sets breakpoint and resumes execution several times.
+            Before hiting the BP program save cache config and after resuming checks its value.
+        """
+        self.select_sub_test(801)
+        # 2 HW + 1 SW flash BP
+        self.bps = ['app_main', 'gpio_set_direction', 'gpio_set_level']
+        for f in self.bps:
+            self.add_bp(f)
+        self.run_to_bp_and_check(dbg.Gdb.TARGET_STOP_REASON_BP, 'gpio_set_direction', ['gpio_set_direction'], outmost_func_name='cache_check_task')
+        for i in range(5):
+            self.run_to_bp_and_check(dbg.Gdb.TARGET_STOP_REASON_BP, 'gpio_set_level', ['gpio_set_level'], outmost_func_name='cache_check_task')
+
 
 ########################################################################
 #              TESTS DEFINITION WITH SPECIAL TESTS                     #
