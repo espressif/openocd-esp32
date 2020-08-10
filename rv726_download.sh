4af410c5260b87013d62dfdda46b15ea8ea9874a
duyi <duyayuan@espressif.com>
2020-08-10 15:07:59 +0800

    Support ocd download ROM code

--- rv726_download.sh
+++ rv726_download.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 src/openocd -s tcl -f board/esprv_ftdi.cfg
+
+#Use the following command to download via ocd
+#src/openocd -s tcl -f board/esprv_ftdi.cfg -c "init; esp32_attach_proc_pro2"
