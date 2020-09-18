3c4519f9153052a65edb053f57cd16dad5e43b0f
Alexey Gerenkov <alexey@espressif.com>
2020-09-18 21:03:48 +0300

    esp/tests: Fixes threads backtraces test

--- testing/esp/test_apps/gen_ut_app/main/thread_check_funcs.c
+++ testing/esp/test_apps/gen_ut_app/main/thread_check_funcs.c
@@ -11,6 +11,8 @@ const static char *TAG = "threads";
 
 #define MAX_TASK_COUNT 6
 
+volatile static int s_task_flags = 0;
+
 static void test_check_nops(int param)
 {
     asm("   nop;");
@@ -42,8 +44,9 @@ static void __attribute__((noinline)) go_to_level_task1(int frame_level)
     if (--frame_level > 0) {
         go_to_level_task1(frame_level);
     }
+    TEST_BREAK_LOC(go_to_level_task1);
     while(1) {
-        s_tmp_ln++; TEST_BREAK_LOC(go_to_level_task1);
+        s_task_flags |= (1 << 0);
     }
 }
 
@@ -52,8 +55,9 @@ static void __attribute__((noinline)) go_to_level_task2(int frame_level)
     if (--frame_level > 0) {
         go_to_level_task2(frame_level);
     }
+    TEST_BREAK_LOC(go_to_level_task2);
     while(1) {
-        s_tmp_ln++; TEST_BREAK_LOC(go_to_level_task2);
+        s_task_flags |= (1 << 1);
     }
 }
 
@@ -62,43 +66,55 @@ static void __attribute__((noinline)) go_to_level_task3(int frame_level)
     if (--frame_level > 0) {
         go_to_level_task3(frame_level);
     }
+    TEST_BREAK_LOC(go_to_level_task3);
     while(1) {
-        s_tmp_ln++; TEST_BREAK_LOC(go_to_level_task3);
+        s_task_flags |= (1 << 2);
     }
 }
 
-void check_backtrace_task3(void *pvParameter)
+static void check_backtrace_task3(void *pvParameter)
 {
     vTaskDelay(100);
     go_to_level_task3((int)pvParameter);
 }
 
-void check_backtrace_task2(void *pvParameter)
+static void check_backtrace_task2(void *pvParameter)
 {
     vTaskDelay(100);
     go_to_level_task2((int)pvParameter);
 }
 
-void check_backtrace_task1(void *pvParameter)
+static void check_backtrace_task1(void *pvParameter)
 {
     vTaskDelay(100);
     go_to_level_task1((int)pvParameter);
+}
+
+static void check_backtrace_test_done(void)
+{
+    ESP_LOGI(TAG, "Backtrace Thread test is done");
+}
+
+static void check_backtrace_ctrl_task(void *pvParameter)
+{
+    xTaskCreatePinnedToCore(&check_backtrace_task1, "check_bt_task1", 2048, (void*)3, 5, NULL, 0);
+    xTaskCreatePinnedToCore(&check_backtrace_task2, "check_bt_task2", 2048, (void*)7, 5, NULL, 0);
+    xTaskCreatePinnedToCore(&check_backtrace_task3, "check_bt_task3", 2048, (void*)5, 5, NULL, portNUM_PROCESSORS-1);
+    /* wait untill all tasks are in dedicated loops */
+    while (s_task_flags < 0x7) {
+        vTaskDelay(10);
+    }
+    check_backtrace_test_done();
 }
 
 ut_result_t thread_test_do(int test_num)
 {
     switch (test_num) {
         case 400:
-            xTaskCreatePinnedToCore(&check_backtrace_task1, "check_bt_task1", 2048, (void*)3, 5, NULL, 0);
-            xTaskCreatePinnedToCore(&check_backtrace_task2, "check_bt_task2", 2048, (void*)7, 5, NULL, 0);
-#if CONFIG_FREERTOS_UNICORE
-            xTaskCreatePinnedToCore(&check_backtrace_task3, "check_bt_task3", 2048, (void*)5, 5, NULL, 0);
-#else
-            xTaskCreatePinnedToCore(&check_backtrace_task3, "check_bt_task3", 2048, (void*)5, 5, NULL, 1);
-#endif
+            xTaskCreatePinnedToCore(&check_backtrace_ctrl_task, "check_bt_ctrl_task", 2048, NULL, 5, NULL, 0);
             break;
         case 401:
-            ESP_LOGE(TAG, "Start Thread tests!");
+            ESP_LOGI(TAG, "Start Thread tests!");
             for (int i=0 ; i< 6 ; i++)
             {
                 char task_name[64];
