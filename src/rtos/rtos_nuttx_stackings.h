1c4241a37cab9f3e5ed23a18d59c1242eb164212
Abdelatif Guettouche <abdelatif.guettouche@espressif.com>
2022-04-05 16:37:21 +0200

    rtos/nuttx: Add ESP32-S2 stacking info.
    Signed-off-by: Abdelatif Guettouche <abdelatif.guettouche@espressif.com>

--- src/rtos/rtos_nuttx_stackings.h
+++ src/rtos/rtos_nuttx_stackings.h
@@ -27,5 +27,6 @@
 extern const struct rtos_register_stacking nuttx_stacking_cortex_m;
 extern const struct rtos_register_stacking nuttx_stacking_cortex_m_fpu;
 extern const struct rtos_register_stacking nuttx_esp32_stacking;
+extern const struct rtos_register_stacking nuttx_esp32s2_stacking;
 
 #endif	/* INCLUDED_RTOS_NUTTX_STACKINGS_H */
