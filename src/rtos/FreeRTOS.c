5d3584fc1b1a2ddcece7614bd519af905471c0a2
Antonio Borneo <borneo.antonio@gmail.com>
2022-09-19 23:14:41 +0200

    nds32: drop it, together with aice adapter driver
    The target nds32 and its companion adapter aice have not received
    any real improvement since 2013.
    It has been hard to keep them aligned during the evolution of
    OpenOCD code, with no way for maintainers to really check if they
    are still working.
    No real documentation is present for them in OpenOCD.
    The nds32 code triggers ~50 errors/warnings with scan-build.
    
    The arch nds32 has been dropped from Linux kernel v5.18-rc1.
    
    For all the reasons above, this code has been deprecated with
    commit 2e5df83de7f2 ("nds32: deprecate it, together with aice
    adapter driver") and tagged to be dropped before v0.13.0.
    
    Let it r.i.p. in OpenOCD git history.
    
    While there, drop from checkpatch list the camelcase symbols that
    where only used in this code.
    
    Change-Id: Ide52a217f2228e9da2f1cc5036c48f3536f26952
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/7382
    Tested-by: jenkins

--- src/rtos/FreeRTOS.c
+++ src/rtos/FreeRTOS.c
@@ -91,23 +91,6 @@ static const struct freertos_params freertos_params_list[] = {
 		NULL,					/* fn to pick stacking_info */
 	},
 	{
-		"nds32_v3",				/* target_name */
-		4,						/* thread_count_width; */
-		4,						/* pointer_width; */
-		16,						/* list_next_offset; */
-		8,						/* list_end_offset; */
-		20,						/* list_width; */
-		8,						/* list_elem_next_offset; */
-		12,						/* list_elem_content_offset */
-		0,						/* thread_stack_offset; */
-		52,						/* thread_name_offset; */
-		4,						/* thread_counter_width */
-		&rtos_standard_nds32_n1068_stacking,	/* stacking_info */
-		&rtos_standard_cortex_m4f_stacking,
-		&rtos_standard_cortex_m4f_fpu_stacking,
-		NULL,					/* fn to pick stacking_info */
-	},
-	{
 		"esp32",				/* target_name */
 		4,						/* thread_count_width; */
 		4,						/* pointer_width; */
