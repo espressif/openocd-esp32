e7ea795332702ee9fc1b11b828613600bf8552b8
Mark Zhuang <mark.zhuang@spacemit.com>
2023-07-12 22:52:33 +0800

    target/riscv: update some macro
    1. update RISCV_MAX_HARTS to 2^20 according to SPEC
    2. remove RISCV_MAX_REGISTERS, it's not used anywhere anymore
    3. add parentheses
    
    Change-Id: Iadf0fa1ba3bbe5b9420b8430883e140db87f4f9e
    Signed-off-by: Mark Zhuang <mark.zhuang@spacemit.com>

--- src/target/espressif/esp_riscv_algorithm.h
+++ src/target/espressif/esp_riscv_algorithm.h
@@ -14,6 +14,9 @@
 /** Index of the first user-defined algo arg. @see algorithm_stub */
 #define ESP_RISCV_STUB_ARGS_FUNC_START  2
 
+/* This is not defined in riscv.h anymore */
+#define ESP_RISCV_MAX_REGISTERS	5000
+
 /**
  * RISCV algorithm data.
  */
@@ -21,8 +24,8 @@ struct esp_riscv_algorithm {
 	/** Registers with numbers below and including this number will be backuped before algo start.
 	 * Set to GDB_REGNO_COUNT-1 to save all existing registers. @see enum gdb_regno. */
 	size_t max_saved_reg;
-	uint64_t saved_registers[RISCV_MAX_REGISTERS];
-	bool valid_saved_registers[RISCV_MAX_REGISTERS];
+	uint64_t saved_registers[ESP_RISCV_MAX_REGISTERS];
+	bool valid_saved_registers[ESP_RISCV_MAX_REGISTERS];
 	uint32_t saved_assist_debug_monitor_register;
 };
 
