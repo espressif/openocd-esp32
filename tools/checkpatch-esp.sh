4b1e752c62c2634b53c7ab5173fa05833d0d7899
Samuel Obuch <samuel.obuch@espressif.com>
2024-12-19 13:51:18 +0100

    checkpatch: add option for excluding directories

--- tools/checkpatch-esp.sh
+++ tools/checkpatch-esp.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-cnt=$(git rev-list --count HEAD ^origin/master)
-git diff HEAD~${cnt} \
-            | filterdiff \
-                -x "a/src/jtag/drivers/libjaylink/*" \
-                -x "a/tools/git2cl/*" \
-                -x "a/.gitlab/*" \
-                -x "a/HACKING" \
-                -x "a/testing/esp/*" \
-            | ./tools/scripts/checkpatch.pl --no-signoff \
-            --ignore FUNCTION_ARGUMENTS --ignore CAMELCASE --ignore LONG_LINE_STRING
+since=${1:-origin/master}
+tools/scripts/checkpatch.pl --git ${since}..
