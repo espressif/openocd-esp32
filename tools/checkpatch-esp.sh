937f24c2d80154e604d232ac0c9179f1220e9326
Erhan Kurubas <erhan.kurubas@espressif.com>
2024-12-27 20:28:00 +0300

    checkpatch: fix master branch "no commit" error
    Checkpatch-ignore:SPDX_LICENSE_TAG

--- tools/checkpatch-esp.sh
+++ tools/checkpatch-esp.sh
@@ -2,4 +2,9 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 since=${1:-origin/master}
-tools/scripts/checkpatch.pl --git ${since}..
+commit_count=$(git rev-list --count "${since}..HEAD")
+if [ "$commit_count" -eq 0 ]; then
+    echo "No new commits between '${since}' and 'master'."
+    exit 0
+fi
+tools/scripts/checkpatch.pl --git ${since}..HEAD
