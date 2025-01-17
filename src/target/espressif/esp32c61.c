8d84fc7e32e8b12876ee0fd343f2cf49990dae6f
Samuel Obuch <samuel.obuch@espressif.com>
2025-01-17 12:46:26 +0100

    target/esp32c[5|61]: remove user counters
    This workaround fixes register access from GDB,
    when the target is in user mode.

--- src/target/espressif/esp32c61.c
+++ src/target/espressif/esp32c61.c
@@ -150,8 +150,9 @@ static const char *esp32c61_csrs[] = {
 
 static const char *esp32c61_ro_csrs[] = {
 	/* read-only CSRs, cannot be save/restored as the write would fail */
-	"cycle", "time", "instreth", "cycleh", "instret", "timeh",
-	"hpmcounter8", "hpmcounter9", "hpmcounter13", "hpmcounter8h", "hpmcounter9h", "hpmcounter13h",
+	/* TODO: Workaround solution for OCD-1066. */
+	// "cycle", "time", "instreth", "cycleh", "instret", "timeh",
+	// "hpmcounter8", "hpmcounter9", "hpmcounter13", "hpmcounter8h", "hpmcounter9h", "hpmcounter13h",
 	"mintstatus",
 	/* custom exposed CSRs will start with 'csr_' prefix*/
 	"csr_mcpuid",
