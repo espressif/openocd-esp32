e3511223c3cb173f0a2dc0e2f274690c8a676614
Samuel Obuch <samuel.obuch@espressif.com>
2025-02-04 15:09:56 +0100

    target/esp32[c5|c61|p4]: move CSR changes from generated encoding header to target files

--- src/target/espressif/esp32c5.c
+++ src/target/espressif/esp32c5.c
@@ -133,7 +133,7 @@ static const struct esp_flash_breakpoint_ops esp32c5_flash_brp_ops = {
 
 static const char *esp32c5_csrs[] = {
 	"mideleg", "medeleg", "mie", "mip", "jvt", "mtvt",
-	"mintthresh", "mnxti", "mscratchcsw", "mscratchcswl",
+	"mnxti", "mscratchcsw", "mscratchcswl",
 	"mcycle", "mcycleh", "minstret", "minstreth",
 	"mhpmevent8", "mhpmevent9", "mhpmevent13",
 	"mhpmcounter8", "mhpmcounter9", "mhpmcounter13", "mhpmcounter8h", "mhpmcounter9h", "mhpmcounter13h",
@@ -145,7 +145,7 @@ static const char *esp32c5_csrs[] = {
 	"csr_pma_addr2", "csr_pma_addr3", "csr_pma_addr4", "csr_pma_addr5", "csr_pma_addr6", "csr_pma_addr7",
 	"csr_pma_addr8", "csr_pma_addr9", "csr_pma_addr10", "csr_pma_addr11", "csr_pma_addr12", "csr_pma_addr13",
 	"csr_pma_addr14", "csr_pma_addr15", "csr_mxstatus", "csr_mhcr", "csr_mhint", "csr_mexstatus",
-	"csr_mclicbase", "csr_mraddr", "csr_mnmicause",
+	"csr_mclicbase", "csr_mraddr", "csr_mnmicause", "csr_mintthresh",
 };
 
 static const char *esp32c5_ro_csrs[] = {
@@ -153,9 +153,8 @@ static const char *esp32c5_ro_csrs[] = {
 	/* TODO: Workaround solution for OCD-1066. */
 	// "cycle", "time", "instreth", "cycleh", "instret", "timeh",
 	// "hpmcounter8", "hpmcounter9", "hpmcounter13", "hpmcounter8h", "hpmcounter9h", "hpmcounter13h",
-	"mintstatus",
 	/* custom exposed CSRs will start with 'csr_' prefix*/
-	"csr_mcpuid",
+	"csr_mintstatus", "csr_mcpuid",
 };
 
 static int esp32c5_target_create(struct target *target, Jim_Interp *interp)
