0890d5eea9389d62859904fdaa0cc5411713f381
Alexey Gerenkov <alexey@espressif.com>
2025-05-19 19:30:14 +0300

    tools: Fix Espressif boards detecting script

--- tools/esp_detect_config.py
+++ tools/esp_detect_config.py
@@ -6,10 +6,18 @@ import json
 import argparse
 import logging
 import os.path
+import os
 import socket
 import time
 import copy
 import tempfile
+import sys
+
+class OpenOcdRunError(RuntimeError):
+    pass
+
+class OpenOcdInstanceConflictError(OpenOcdRunError):
+    pass
 
 
 class OpenOcd:
@@ -19,50 +27,97 @@ class OpenOcd:
     TAPINS_NARSEL_DATALEN   = 32
     XDMREG_OCDID_NAR        = 0x40
 
-    def __init__(self, oocd_path, scripts_dir, iface_config, host="127.0.0.1", port=6666, start_tmo=1, iface_cmd="", env=None):
+    def __init__(self, oocd_path, scripts_dir, iface_config, host="127.0.0.1", port=10677, iface_cmd="", env=None, log_lvl=2, data_tmo=2.0):
         self.host       = host
         self.port       = port
         self.buf_size   = 4096
         self.sock       = None
         self.iface_cmd  = iface_cmd
-
+        self.data_tmo   = data_tmo
+        # check for running instance, it can be due to active debug session
+        pid = self._instance_is_running(oocd_path)
+        if pid:
+            logging.critical("Looks like another OpenOCD instance is running: pid = %d!", pid)
+            raise OpenOcdInstanceConflictError()
+        fp = tempfile.NamedTemporaryFile(delete=False)
+        self.logfilename = fp.name
+        fp.close()
+        if os.name == 'nt':
+            self.logfilename = self.logfilename.replace('\\', '/')
         run_args = [oocd_path]
         if len(scripts_dir):
             run_args += ["-s", scripts_dir]
-        run_args += ["-f", iface_config]
+        run_args += ["-f", iface_config, f"-d{log_lvl}", "-l", self.logfilename,
+                     "-c", f'gdb port disabled; telnet port disabled; tcl port {self.port}; init']
         logging.debug("Run OpenOCD with args: %s", run_args)
-        self.proc = subprocess.Popen(args=run_args, bufsize=0, stdout=subprocess.PIPE, stdin=None,
-                                     stderr=subprocess.STDOUT, universal_newlines=True, env=env)
+        self.proc = subprocess.Popen(args=run_args, stdout=subprocess.PIPE, stdin=None, stderr=subprocess.STDOUT, env=env)
         if self.proc.poll():
             logging.error("Failed to start OpenOCD %d!", self.proc.returncode)
             self.readout_all_output()
-            raise RuntimeError
-        time.sleep(start_tmo)
-        # logging.error(self.proc.stdout.read(20))
+            self.cleanup()
+            raise OpenOcdRunError()
+
+    def _instance_is_running(self, oocd_path):
+        try:
+            import psutil
+        except:
+            logging.warning("""
+                            For correct operation this script should not be run when another OpenOCD instance is running.
+                            Please, install 'psutils' Python package to be able to detect such problems.
+                            """)
+            return None
+        oocd_bin = os.path.basename(oocd_path)
+        for proc in psutil.process_iter():
+            try:
+                if oocd_bin.lower() in proc.name().lower():
+                    return proc.pid
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return None
 
     def readout_all_output(self):
         try:
-            outs,_ = self.proc.communicate(timeout=5)
-        except subprocess.TimeoutExpired:
-            logging.error("Failed to wait for OpenOCD output! Kill it.")
-            self.proc.kill()
-            outs,_ = self.proc.communicate()
-        logging.debug(outs)
+            self.proc.wait(0.3)
+        except:
+            logging.warning("Failed to wait for OpenOCD exit!")
+        with open(self.logfilename, mode='rb') as f:
+            logging.debug(f.read().decode("utf-8"))
 
-    def connect(self):
+    def cleanup(self):
+        if not os.path.exists(self.logfilename):
+            return
         try:
-            self._connect()
-        except ConnectionRefusedError:
-            logging.warning("Failed to connect to OpenOCD!")
-            self.proc.kill()
-            self.readout_all_output()
-            raise ConnectionRefusedError
+            os.remove(self.logfilename)
+        except Exception as e:
+            logging.warning("Failed to remove log file '%s': %s!", self.logfilename, e)
+
+    def connect(self, tmo=2):
+        logging.debug("Connect to %s:%d", self.host, self.port)
+        deadline = time.time() + tmo
+        while(True):
+            try:
+                self._connect(tmo=0.5)
+            except:
+                ret_code = self.proc.poll()
+                if ret_code or time.time() >= deadline:
+                    logging.info(f"Failed to connect to OpenOCD (exit code {ret_code}).")
+                    if ret_code:
+                        self.proc.kill()
+                    self.readout_all_output()
+                    raise
+            else:
+                break
+        logging.debug("Connected to %s:%d", self.host, self.port)
         return self
 
     def disconnect(self):
-        self._disconnect()
-        self.proc.kill()
-        self.readout_all_output()
+        try:
+            self._disconnect()
+        except:
+            raise
+        finally:
+            self.proc.kill()
+            self.readout_all_output()
 
     def __enter__(self):
         self.connect()
@@ -70,10 +125,8 @@ class OpenOcd:
     def __exit__(self, type, value, traceback):
         self.disconnect()
 
-    def _connect(self):
-        logging.debug("Try to connect to %s:%d", self.host, self.port)
-        self.sock = socket.create_connection((self.host, self.port), timeout=5)
-        logging.debug("Connected to %s:%d", self.host, self.port)
+    def _connect(self, tmo=0.5):
+        self.sock = socket.create_connection((self.host, self.port), timeout=tmo)
 
     def _disconnect(self):
         if not self.sock:
@@ -88,24 +141,28 @@ class OpenOcd:
         """Send a command string to TCL RPC. Return the result that was read."""
         data = (cmd + OpenOcd.COMMAND_TOKEN).encode("utf-8")
         logging.debug("-> {%s}", data)
+        self.sock.settimeout(self.data_tmo)
         self.sock.send(data)
         return self._recv()
 
     def _recv(self):
         """Read from the stream until the token (\x1a) was received."""
         data = bytes()
+        self.sock.settimeout(0.5)
+        deadline = time.time() + self.data_tmo
         while True:
-            chunk = self.sock.recv(self.buf_size)
-            data += chunk
-            if bytes(OpenOcd.COMMAND_TOKEN, encoding="utf-8") in chunk:
-                break
-
+            try:
+                chunk = self.sock.recv(self.buf_size)
+                data += chunk
+                if bytes(OpenOcd.COMMAND_TOKEN, encoding="utf-8") in chunk:
+                    break
+            except TimeoutError:
+                pass
+            if time.time() >= deadline:
+                raise TimeoutError
         logging.debug("<- {%s}", data)
-
         data = data.decode("utf-8").strip()
-        data = data[:-1] # strip trailing \x1a
-
-        return data
+        return data[:-1] # strip trailing \x1a
 
     def is_xtensa(self, idcode):
         return idcode == "0x120034e5"
@@ -148,18 +205,24 @@ class OpenOcd:
         logging.debug(dev_path)
         return dev_path
 
-class OpenOcdRunError(RuntimeError):
-    pass
 
-def detect_and_populate_config(oocd, scripts, config_file, host, port, start_delay,
+def detect_and_populate_config(oocd, scripts, log_lvl, data_tmo, config_file, host, port,
                                iface_id, iface_cmd, usb_location, esp_cfg):
+    my_env = os.environ.copy()
+    if usb_location:
+        my_env["OPENOCD_USB_ADAPTER_LOCATION"] = usb_location
+
+    ocd = None
     try:
-        my_env = os.environ.copy()
-        if usb_location:
-            my_env["OPENOCD_USB_ADAPTER_LOCATION"] = usb_location
-        ocd = OpenOcd(oocd, scripts, config_file, host, port, start_delay, iface_cmd, env=my_env)
-        ocd.connect()
+        ocd = OpenOcd(oocd, scripts, config_file, host, port, iface_cmd, env=my_env, log_lvl=log_lvl, data_tmo=data_tmo)
+        ocd.connect(tmo=5)
+    except OpenOcdInstanceConflictError:
+        # no need to call cleanup()
+        # Check for OpenOCD running instance is done before any things which need cleanup
+        raise
     except:
+        if ocd:
+            ocd.cleanup()
         raise OpenOcdRunError()
 
     try:
@@ -192,14 +255,17 @@ def detect_and_populate_config(oocd, scripts, config_file, host, port, start_del
                                 break
                         if not duplicate:
                             found = True
-                            logging.info("Found board %s", cfg_board["name"])
-                            iface_boards.append(cfg_board)
-                            iface_boards[-1]["location"] = "usb://" + (usb_location if usb_location else curr_dev)
+                            loc = usb_location if usb_location else curr_dev
+                            logging.info("Found board %s @ %s", cfg_board["name"], loc)
+                            board_entry = copy.deepcopy(cfg_board)
+                            board_entry["location"] = f"usb://{loc}"
+                            iface_boards.append(board_entry)
                 if not found:
                     logging.debug("No board entry found for idcode %s target %s @ iface %s!",
                                     tgt["idcode"], tgt["name"], iface_id)
     finally:
         ocd.disconnect()
+        ocd.cleanup()
     return iface_boards,devices
 
 
@@ -207,17 +273,18 @@ def main():  # type: () -> None
 
     parser = argparse.ArgumentParser('Espressif OpenOCD Config Detecting Tool')
 
-    parser.add_argument('--esp-config', '-c', help='Path to Espressif OpenOCD config json file', type=str)
+    parser.add_argument('--esp-config', '-c', help='Path to Espressif OpenOCD config json file', type=str, required=True)
     parser.add_argument('--oocd', '-b', help='Path to OpenOCD binary', type=str, default="openocd")
     parser.add_argument('--scripts', '-s', help='Path to OpenOCD scripts', type=str, default="")
-    parser.add_argument('--debug', '-d', help='Debug level: 0-4', type=int, default=1)
-    parser.add_argument('--start-delay', '-r', help='OpenOCD startup time', type=int, default=1)
+    parser.add_argument('--debug', '-d', help='Debug level: 0-5', type=int, default=2)
     parser.add_argument('--host', '-a', help='OpenOCD host IP addr', type=str, default="127.0.0.1")
-    parser.add_argument('--port', '-p', help='OpenOCD TCL port number', type=int, default=6666)
+    parser.add_argument('--port', '-p', help='OpenOCD TCL port number', type=int, default=10677)
     parser.add_argument('--output', '-o', help='Output filename', type=str, default="stdout")
+    parser.add_argument('--timeout', '-t', help='Communication timeout', type=float, default=3.0)
 
     args = parser.parse_args()
 
+    ocd_log_level = 2
     if args.debug == 0:
         log_level = logging.CRITICAL
     elif args.debug == 1:
@@ -228,6 +295,8 @@ def main():  # type: () -> None
         log_level = logging.INFO
     else:
         log_level = logging.DEBUG
+        if args.debug > 4:
+            ocd_log_level = args.debug - 2
     logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
 
     with open(args.esp_config, "r") as f:
@@ -241,26 +310,32 @@ def main():  # type: () -> None
         logging.info("Check interface %s", iface)
         extra_devs = []
         try:
-            iface_boards,extra_devs = detect_and_populate_config(args.oocd, args.scripts,
+            iface_boards,extra_devs = detect_and_populate_config(args.oocd, args.scripts, ocd_log_level, args.timeout,
                                                                  os.path.join("interface", iface["config_file"]),
-                                                                 args.host, args.port, args.start_delay,
+                                                                 args.host, args.port,
                                                                  iface["id"], iface["command"], None, esp_cfg)
         except OpenOcdRunError:
-            logging.warning("Skip interface %s", iface)
+            logging.info("Skip interface %s", iface)
             continue
+        except OpenOcdInstanceConflictError:
+            # error message is already printed
+            sys.exit(-1)
         cfg_out["boards"].extend(iface_boards)
 
         for dev in extra_devs:
             logging.info("Check interface %s @ %s", iface, dev)
             try:
-                iface_boards,_ = detect_and_populate_config(args.oocd, args.scripts,
+                extra_iface_boards,_ = detect_and_populate_config(args.oocd, args.scripts, ocd_log_level, args.timeout,
                                                             os.path.join("interface", iface["config_file"]),
-                                                            args.host, args.port, args.start_delay,
+                                                            args.host, args.port,
                                                             iface["id"], iface["command"], dev, esp_cfg)
             except OpenOcdRunError:
-                logging.warning("Skip interface %s @ %s", iface, dev)
+                logging.info("Skip interface %s @ %s", iface, dev)
                 continue
-            cfg_out["boards"].extend(iface_boards)
+            except OpenOcdInstanceConflictError:
+                # error message is already printed
+                sys.exit(-1)
+            cfg_out["boards"].extend(extra_iface_boards)
 
     if args.output == "stdout":
         print(json.dumps(cfg_out))
