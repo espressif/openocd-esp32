42e446d81fdd59f8fdf2ada74a62a4c2048d881d
Antonio Borneo <borneo.antonio@gmail.com>
2026-05-18 10:56:07 +0200

    target: profile: avoid division by zero
    Issue detected by scan-build.
    
    The function target_profiling() can return with num_of_samples==0
    and this will trigger a division by zero.
    
    Check the value of num_of_samples before continuing.
    While there, simplify the return path and the return value.
    
    Fixes: dcf628298534 ("target: filter and sort pc samples before storing")
    Change-Id: I74faa7de5c7fac6f8c862e338d2de5492a0dd14a
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/9659
    Tested-by: jenkins
    Reviewed-by: Richard Allen <rsaxvc@gmail.com>

--- src/target/target.c
+++ src/target/target.c
@@ -4370,6 +4370,12 @@ COMMAND_HANDLER(handle_profile_command)
 		return retval;
 	}
 
+	if (!num_of_samples) {
+		command_print(CMD, "Wrote no samples");
+		free(samples);
+		return ERROR_OK;
+	}
+
 	if (with_range) {
 		uint32_t num_filtered_samples = 0;
 		for (uint32_t in = 0; in < num_of_samples; ++in) {
@@ -4381,40 +4387,42 @@ COMMAND_HANDLER(handle_profile_command)
 		if (duration_ms < 1)
 			duration_ms = 0;
 		num_of_samples = num_filtered_samples;
-	}
 
-	if (num_of_samples) {
-		qsort(samples, num_of_samples, sizeof(samples[0]), compare_pc32);
-
-		uint32_t *counts = malloc(sizeof(uint32_t) * num_of_samples);
-		if (!counts) {
-			LOG_ERROR("No memory to store sample counts.");
+		if (!num_of_samples) {
+			command_print(CMD, "Wrote no samples in the requested range");
 			free(samples);
-			return ERROR_FAIL;
+			return ERROR_OK;
 		}
-		counts[0] = 1;
-		uint32_t num_unique_samples = 1;
-		for (uint32_t in = 1; in < num_of_samples; ++in) {
-			uint32_t sample = samples[in];
-			if (sample != samples[num_unique_samples - 1]) {
-				samples[num_unique_samples] = sample;
-				counts[num_unique_samples] = 1;
-				num_unique_samples++;
-			} else {
-				counts[num_unique_samples - 1]++;
-			}
-		}
-
-		float sample_rate = num_of_samples / (duration_ms / 1000.0);
-		write_gmon(samples, counts, num_unique_samples, CMD_ARGV[1], target, duration_ms, sample_rate);
-		command_print(CMD, "Wrote %s", CMD_ARGV[1]);
-		free(counts);
-	} else {
-		command_print(CMD, "Wrote no samples");
 	}
+
+	qsort(samples, num_of_samples, sizeof(samples[0]), compare_pc32);
+
+	uint32_t *counts = malloc(sizeof(uint32_t) * num_of_samples);
+	if (!counts) {
+		LOG_ERROR("No memory to store sample counts.");
+		free(samples);
+		return ERROR_FAIL;
+	}
+	counts[0] = 1;
+	uint32_t num_unique_samples = 1;
+	for (uint32_t in = 1; in < num_of_samples; ++in) {
+		uint32_t sample = samples[in];
+		if (sample != samples[num_unique_samples - 1]) {
+			samples[num_unique_samples] = sample;
+			counts[num_unique_samples] = 1;
+			num_unique_samples++;
+		} else {
+			counts[num_unique_samples - 1]++;
+		}
+	}
+
+	float sample_rate = num_of_samples / (duration_ms / 1000.0);
+	write_gmon(samples, counts, num_unique_samples, CMD_ARGV[1], target, duration_ms, sample_rate);
+	command_print(CMD, "Wrote %s", CMD_ARGV[1]);
+	free(counts);
 
 	free(samples);
-	return retval;
+	return ERROR_OK;
 }
 
 COMMAND_HANDLER(handle_target_read_memory)
