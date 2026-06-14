ce23eba73e2ad8c63f8aa326613467a085af61b7
Grant Ramsay <grant.ramsay@hotmail.com>
2026-06-15 09:55:40 +1200

    time_support: Use monotonic time for timeval_ms
    gettimeofday is susceptible to NTP adjustments and marked as obsolete.
    Replace timeval_ms implementation with suitable monotonic time source
    and fall back to gettimeofday if needed
    
    Change-Id: Iba21bfb023a9df1e668175bea327467bbd5b45b4
    Signed-off-by: Grant Ramsay <grant.ramsay@hotmail.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/9743
    Reviewed-by: Tomas Vanek <vanekt@fbl.cz>
    Tested-by: jenkins
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/helper/time_support_common.c
+++ src/helper/time_support_common.c
@@ -9,6 +9,9 @@
  *                                                                         *
  *   Copyright (C) 2008 by Spencer Oliver                                  *
  *   spen@spen-soft.co.uk                                                  *
+ *                                                                         *
+ *   Copyright (C) 2026 by Grant Ramsay                                    *
+ *   grant.ramsay@hotmail.com                                              *
  ***************************************************************************/
 
 #ifdef HAVE_CONFIG_H
@@ -17,14 +20,44 @@
 
 #include "time_support.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 /* simple and low overhead fetching of ms counter. Use only
  * the difference between ms counters returned from this fn.
+ * Use QueryPerformanceCounter for Windows, else
+ * clock_gettime with CLOCK_MONOTONIC_RAW if available, else
+ * clock_gettime with CLOCK_MONOTONIC if available, else
+ * fallback to gettimeofday (susceptible to NTP adjustments)
  */
 int64_t timeval_ms(void)
 {
+#ifdef _WIN32
+	static LARGE_INTEGER frequency;
+	LARGE_INTEGER now;
+	// frequency is static and only read once on the first call
+	if (frequency.QuadPart == 0 && !QueryPerformanceFrequency(&frequency))
+		return -1;
+	if (!QueryPerformanceCounter(&now))
+		return -1;
+	return (now.QuadPart * 1000) / frequency.QuadPart;
+#elif defined(HAVE_CLOCK_GETTIME)
+#ifdef CLOCK_MONOTONIC_RAW
+	clockid_t clk_id = CLOCK_MONOTONIC_RAW;
+#else
+	clockid_t clk_id = CLOCK_MONOTONIC;
+#endif
+	struct timespec now;
+	int retval = clock_gettime(clk_id, &now);
+	if (retval < 0)
+		return retval;
+	return (int64_t)now.tv_sec * 1000 + now.tv_nsec / 1000000;
+#else
 	struct timeval now;
 	int retval = gettimeofday(&now, NULL);
 	if (retval < 0)
 		return retval;
 	return (int64_t)now.tv_sec * 1000 + now.tv_usec / 1000;
+#endif
 }
